ba9a849265d72587eb4f481e2566ff2ba4030710
LUFENGZHAN <xiaozhan2000423@163.com>
2023-10-03 18:36:35 +0800

    init

---
+++ pages/home/home.vue
@@ -0,0 +1,18 @@
+<template>
+	<view class="content">
+		<video src="https://mp-2a0e68a1-4e42-4285-b25e-1462368efa7f.cdn.bspapp.com/videos/hy.mp4"></video>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+</style>
---
+++ unpackage/dist/dev/.nvue/app.js
@@ -0,0 +1,5 @@
+Promise.resolve("./pages/list/list.js").then((res) => {
+  res.length;
+});
+Promise.resolve("./app.css.js").then(() => {
+});
